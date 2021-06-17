89edf9ce7b4bd1d7ef942d28b804e61a507df469
YamkelaMathontsi <82159030+YamkelaMathontsi@users.noreply.github.com>
2021-06-17 11:12:56 +0200

    Add files via upload

---
+++ js pages/java.js
@@ -0,0 +1,5 @@
+function toggleNavbar() {
+  document.getElementsByClassName("navbar-links")[0].classList.toggle("active");
+  // let navBar = document.querySelector("navbar-links");
+  // navBar.style.display = "flex";
+}
